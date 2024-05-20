bab86018d00897bcde79ea3039b9f572a8062999
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-20 09:57:24 -0300

    forms revisado mais separação da page

---
+++ my-app/src/app/Integracao/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Pages from '../Func/page';
+import './int.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-div">
+        <h1>Cadastro Clientes</h1>
+      </div>
+      <Pages />
+    </div>
+  );
+}
+
+export default App;
